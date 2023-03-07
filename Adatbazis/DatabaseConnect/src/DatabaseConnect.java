b53374a4b3ffb362d631bcca5c5b9903314cd65a
Peter Kiraly <peter.kiral@gmail.com>
2023-03-07 17:59:14 +0100

    database ready

---
+++ Adatbazis/DatabaseConnect/src/DatabaseConnect.java
@@ -0,0 +1,18 @@
+/* 
+
+amire szükség van:
+database connector
+connection string // ezt nekünk kell létrehozni
+    - database driver 
+        https://mariadb.org/connector-java/all-releases/ -- 2.2.78 (ez stabil...)
+    - path:port
+    - database name
+    - user
+    - password
+
+*/
+public class DatabaseConnect {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
--- Adatbazis/elso.sql
+++ Adatbazis/elso.sql
@@ -1,1 +1,15 @@
-CREATE DATABASE elso;
+CREATE DATABASE szfesti_elso;
+USE szfesti_elso;
+
+CREATE TABLE workers(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50),
+	city VARCHAR(20),
+	salary INT,
+	born DATE
+);
+
+INSERT INTO workers(name, city, salary, born) VALUES
+("Rabsz Olga", "Vác", 450000, "1996-02-21"),
+("Kalap Pál", "Ócsa", 560000, "2001-05-06"),
+("Har Mónika", "Szeged", 490000, "1995-11-12");
